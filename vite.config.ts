8d41585cc950d83a3dbace81c2d0309e726ceeb1
maan <progcode05@gmail.com>
2025-10-21 16:36:21 +0530

    update: new changes to homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,18 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
+export default defineConfig(({ mode }) => (
+  {
+  server: 
+  {
     host: "::",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
+  resolve: 
+  {
+    alias: 
+    {
       "@": path.resolve(__dirname, "./src"),
     },
   },
